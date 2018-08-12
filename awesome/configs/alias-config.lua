945a6e118e3082264a611865bf6c4aeac6e0bfdc
Mattia De Rosa <mattiadr96@gmail.com>
2018-08-12 22:25:55 +0200

    zsh: fixed ln completion, updated ls alias. awesome: added VLC to appnames

--- awesome/configs/alias-config.lua
+++ awesome/configs/alias-config.lua
@@ -12,11 +12,13 @@ appnames["Nm-applet"]            = "NM App"
 appnames["st-256color"]          = "st"
 appnames["Sublime_text"]         = "Sublime"
 appnames["TelegramDesktop"]      = "TG"
+appnames["vlc"]                  = "VLC"
 
 appnames.short = {}
 appnames.short["Chromium"]       = "WEB"
 appnames.short["Eclipse"]        = "DEV"
 appnames.short["qBittorrent"]    = "QBIT"
 appnames.short["Sublime_text"]   = "DEV"
+appnames.short["vlc"]            = "VLC"
 
 return appnames
