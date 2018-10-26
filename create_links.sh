353c2b8e7fbb4aab7f59d807141d33d094ff2e90
Mattia De Rosa <mattiadr96@gmail.com>
2018-10-26 17:24:15 +0200

    added create_links.sh to quickly create soft links to configs in conf-files

---
+++ create_links.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+curr_dir=$(pwd)
+
+cln() {
+	rm -rf "$2"
+	ln -s "$curr_dir/$1" "$2"
+}
+
+#  | conf-files       | real file
+cln awesome            ~/.config/awesome
+cln cower              ~/.config/cower
+cln htop               ~/.config/htop
+cln nano/nanorc        /etc/nanorc
+cln pacman/mirrorlist  /etc/pacman.d/mirrorlist
+cln pacman/pacman.conf /etc/pacman.conf
+cln ranger             ~/.config/ranger
+cln sublime-text-3     ~/.config/sublime-text-3
+cln vlc                ~/.config/vlc
+cln zsh/zshrc          /etc/zsh/zshrc
