8129a345f215dada1f2624cba5777e1c7a2a268e
Mattia De Rosa <mattiadr96@gmail.com>
2019-05-12 18:58:23 +0200

    using sudo in create_links.sh when needed disabled cower link

--- create_links.sh
+++ create_links.sh
@@ -7,19 +7,24 @@ cln() {
 	ln -s "$curr_dir/$1" "$2"
 }
 
+scln() {
+	sudo rm -rf "$2"
+	sudo ln -s "$curr_dir/$1" "$2"
+}
+
 #  | conf-files       | real file
-cln awesome            ~/.config/awesome
-cln cower              ~/.config/cower
-cln git/.gitconfig     ~/.gitconfig
-cln htop               ~/.config/htop
-cln nano/nanorc        /etc/nanorc
-cln pacman/mirrorlist  /etc/pacman.d/mirrorlist
-cln pacman/pacman.conf /etc/pacman.conf
-cln ranger             ~/.config/ranger
-cln sublime-text-3     ~/.config/sublime-text-3
-cln vlc                ~/.config/vlc
-cln xorg/.xinitrc      ~/.xinitrc
-cln xorg/.Xresources   ~/.Xresources
-cln zsh/.zprofile      ~/.zprofile
-cln zsh/zshenv         /etc/zsh/zshenv
-cln zsh/zshrc          /etc/zsh/zshrc
+cln  awesome            ~/.config/awesome
+#cln  cower              ~/.config/cower
+cln  git/.gitconfig     ~/.gitconfig
+cln  htop               ~/.config/htop
+scln nano/nanorc        /etc/nanorc
+scln pacman/mirrorlist  /etc/pacman.d/mirrorlist
+scln pacman/pacman.conf /etc/pacman.conf
+cln  ranger             ~/.config/ranger
+cln  sublime-text-3     ~/.config/sublime-text-3
+cln  vlc                ~/.config/vlc
+cln  xorg/.xinitrc      ~/.xinitrc
+cln  xorg/.Xresources   ~/.Xresources
+cln  zsh/.zprofile      ~/.zprofile
+scln zsh/zshenv         /etc/zsh/zshenv
+scln zsh/zshrc          /etc/zsh/zshrc
