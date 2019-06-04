7211a57b17ea5e111db057685c5fd30e6423e004
Mattia De Rosa <mattiadr96@gmail.com>
2019-06-04 12:47:08 +0200

    [desktop] new: makepkg

--- create_links.sh
+++ create_links.sh
@@ -12,19 +12,20 @@ scln() {
 	sudo ln -s "$curr_dir/$1" "$2"
 }
 
-#  | conf-files       | real file
-cln  awesome            ~/.config/awesome
-#cln  cower              ~/.config/cower
-cln  git/.gitconfig     ~/.gitconfig
-cln  htop               ~/.config/htop
-scln nano/nanorc        /etc/nanorc
-scln pacman/mirrorlist  /etc/pacman.d/mirrorlist
-scln pacman/pacman.conf /etc/pacman.conf
-cln  ranger             ~/.config/ranger
-cln  sublime-text-3     ~/.config/sublime-text-3
-cln  vlc                ~/.config/vlc
-cln  xorg/.xinitrc      ~/.xinitrc
-cln  xorg/.Xresources   ~/.Xresources
-cln  zsh/.zprofile      ~/.zprofile
-scln zsh/zshenv         /etc/zsh/zshenv
-scln zsh/zshrc          /etc/zsh/zshrc
+#    conf-files          | real file
+cln  awesome               ~/.config/awesome
+#cln  cower                 ~/.config/cower
+cln  git/.gitconfig        ~/.gitconfig
+cln  htop                  ~/.config/htop
+scln makepkg/makepkg.conf  /etc/makepkg.conf
+scln nano/nanorc           /etc/nanorc
+scln pacman/mirrorlist     /etc/pacman.d/mirrorlist
+scln pacman/pacman.conf    /etc/pacman.conf
+cln  ranger                ~/.config/ranger
+cln  sublime-text-3        ~/.config/sublime-text-3
+cln  vlc                   ~/.config/vlc
+cln  xorg/.xinitrc         ~/.xinitrc
+cln  xorg/.Xresources      ~/.Xresources
+cln  zsh/.zprofile         ~/.zprofile
+scln zsh/zshenv            /etc/zsh/zshenv
+scln zsh/zshrc             /etc/zsh/zshrc
